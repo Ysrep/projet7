e868e4954b6d2c2876d4532402f45e908dfd3dc1
katsutsani <katsutsani@gmail.com>
2023-01-23 16:24:56 +0100

    color + diagramClass

--- Projet7/Program.cs
+++ Projet7/Program.cs
@@ -1,10 +1,55 @@
-﻿namespace Projet7
+﻿using System.Drawing;
+
+namespace Projet7
 {
     internal class Program
     {
+
+        public int ShortHealth { get; set; }
+
+        //Math.Max(0,_health-amount);
+        //Math.Min(_maxHealth,_health+amount);
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("#####################################################");
+
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("wwwwwwwwwwwwwwwwwwwwwwwwwwwww");
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write("x");
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write("!");
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.WriteLine("--------------------------------------");
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.WriteLine("                                         ");
+
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("wwwwwwwwwwwwwwwwwwwwwwwwwwwww");
+
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+
+
         }
     }
 }
