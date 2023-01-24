0381b8a637b472db599b70d327b39f776440c1eb
Silver_Haze_ <83134606+SilverHaze72@users.noreply.github.com>
2023-01-24 17:04:08 +0100

    Add function
    Add function to NPC and PokemonTeam

--- Projet7/NPC.cs
+++ Projet7/NPC.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace Projet7
 {
     public class NPC
     {
+        public int? teamIdGet { get; }
+        public int? DialogSelection { get; set; }
+        public int? moneyGive { get; set; }
+
+        public int _trainerPosX;
+        public int _trainerPosY;
+
+        public void setTrainerPos(int trainerPosX, int trainerPosY)
+        {
+            _trainerPosX = trainerPosX;
+            _trainerPosY = trainerPosY;
+            return;
+        }
+
     }
 }
--- Projet7/PokemonTeam.cs
+++ Projet7/PokemonTeam.cs
@@ -2,10 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace Projet7
 {
     public class PokemonTeam
     {
+
+        public int _teamId;
+        public bool _isTrainerTeam = false;
+        public int _nbrOfPokemon;
+        public int _lvlOfEachPokemon;
+
+        public void TrainerTeam (int teamId)
+        {
+            _teamId = teamId;
+        }
+
+        public void isTrainerDetection()
+        {
+
+        }
+
     }
 }
