047890d027d3850cfbc810e7cbf45ca851077492
katsutsani <katsutsani@gmail.com>
2023-02-01 11:37:31 +0100

    save and menu

--- Projet7/Program.cs
+++ Projet7/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System.Drawing;
+using System.Numerics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Projet7
 {
@@ -9,29 +12,91 @@ namespace Projet7
 
         //Math.Max(0,_health-amount);
         //Math.Min(_maxHealth,_health+amount);
-
         static void Main(string[] args)
         {
-            Map map = new Map();
-            Player player = new Player(63,218);
-            AllBag allinventory = new AllBag();
+
+            string path = @"player.json";
+
+            Player player = new Player();
             ListConstruct inventory = new ListConstruct();
-            Menu menu = new Menu();
-            inventory.List();
-            map.Init();
-            menu.ShowMenu();
-            while (map.Menu)
+            string path1 = @"inventory.json";
+            Map map = new Map();
+            if (File.Exists(path))
             {
-                map.Menu = menu.Selection();
+                string fileName = "player.json";
+                var jsonString = File.ReadAllText(fileName);
+                player = JsonSerializer.Deserialize<Player>(jsonString)!;
+            }
+            else
+            {
+                player.InitWIthoutJSon(63, 218);
+            }
+
+            if (File.Exists(path1))
+            {
+                string fileName = "inventory.json";
+                var jsonString = File.ReadAllText(fileName);
+                inventory.inventory = JsonSerializer.Deserialize<List<Item>>(jsonString)!;
+            }
+            else
+            {
+                inventory.InitWithoutJSon();
+            }
+            AllBag allinventory = new AllBag();
+            Menu menu = new Menu();
+            map.Init();
+            menu.ShowStartMenu();
+            while (map.StartMenu)
+            {
+                map.Menu = menu.StartMenuSelection();
             }
             map.ShowMap(player.PlayerPos);
-            while (!map.Menu && !map.Inventory)
+            while (!map.StartMenu)
             {
-               player.Inputs(map);
-            }
-            while (!map.Menu && map.Inventory)
-            {
-               allinventory.ItemListDisp(inventory);
+                while (map.Menu)
+                {
+                    menu.ShowMenu();
+                    int index = menu.MenuSelection();
+                    switch (index)
+                    {
+                        case 0:
+                            map.Menu = false;
+                            map.ShowMap(player.PlayerPos);
+                            break;
+                        case 1:
+
+                            break;
+                        case 2:
+                            map.Menu = false;
+                            map.Inventory = true;
+                            break;
+                        case 3:
+                            if (File.Exists(path))
+                            {
+                                File.Delete(path);  
+                            }
+                            string fileName = "player.json";
+                            var jsonString = JsonSerializer.Serialize<Player>(player);
+                            File.WriteAllText(fileName, jsonString);
+                            if (File.Exists(path1))
+                            {
+                                File.Delete(path1);
+                            }
+                            fileName = "inventory.json";
+                            jsonString = JsonSerializer.Serialize<List<Item>>(inventory.inventory);
+                            File.WriteAllText(fileName, jsonString);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                while (map.Inventory)
+                {
+                    map.Inventory = allinventory.ItemListDisp(inventory);
+                    Console.Clear();
+                    map.ShowMap(player.PlayerPos);
+                }
+                player.Inputs(map);
             }
         }
     }
